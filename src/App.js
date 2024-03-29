cd623bdefc1058b79fcd919fc1e635ea0f7e230a
slowerAtom <50114191+slowerAtom@users.noreply.github.com>
2024-03-29 13:27:18 +0000

    Add search result

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 // import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 // import Hello from './components/Hello';
 import InputSearch from './components/InputSearch';
+import SearchResult from './components/SearchResult';
 
 
 function App() {
   return (
-    <div className="relative isolate overflow-hidden h-full bg-slate-200 py-16 sm:py-24 lg:py-32">
+    <div className="relative isolate overflow-hidden h-full bg-slate-200 py-10 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-          <div className="max-w-xl lg:max-w-lg">
+        <div className="mx-auto max-w-2xl gap-x-8 lg:max-w-none">
+          <div className='text-right'>
+            <button class="right-0 mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Add Member
+            </button>
+          </div>
+          <div className="">
             {/* <Hello /> */}
             <InputSearch />
           </div>
+          <SearchResult />
         </div>    
       </div>
     </div>
